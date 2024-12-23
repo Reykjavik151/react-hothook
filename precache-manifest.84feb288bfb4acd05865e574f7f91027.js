0a0ad7c45e65b7838b754b147717f44a4e39c087
Roman Andrieiev <reykjavik151@gmail.com>
2024-12-23 01:30:47 +0000

    Updates

---
+++ precache-manifest.84feb288bfb4acd05865e574f7f91027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb917e24c9b6750a5b3ba3d54f70521",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a3ebd5f9d13abea37fa7",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "04c51b539fc00fc86100",
+    "url": "./static/js/2.5ef37794.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5ef37794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ebd5f9d13abea37fa7",
+    "url": "./static/js/main.92770089.chunk.js"
+  },
+  {
+    "revision": "eaee975215c8b17056f6",
+    "url": "./static/js/runtime-main.c605a77d.js"
+  }
+]);
