d9255b92ed30c9b803cb415be73b3b0273aa78d6
Roman Andrieiev <reykjavik151@gmail.com>
2024-12-23 01:51:10 +0000

    chore: version

--- dist/index.modern.js
+++ dist/index.modern.js
@@ -1,85 +1,37 @@
 import { useState, useEffect, useRef, useCallback } from 'react';
 import { isEmpty, isUndefined, isNull, isEqual } from 'lodash';
 
-// A type of promise-like that resolves synchronously and supports only one observer
-
-const _iteratorSymbol = /*#__PURE__*/ typeof Symbol !== "undefined" ? (Symbol.iterator || (Symbol.iterator = Symbol("Symbol.iterator"))) : "@@iterator";
-
-const _asyncIteratorSymbol = /*#__PURE__*/ typeof Symbol !== "undefined" ? (Symbol.asyncIterator || (Symbol.asyncIterator = Symbol("Symbol.asyncIterator"))) : "@@asyncIterator";
-
-// Asynchronously call a function and send errors to recovery continuation
-function _catch(body, recover) {
-	try {
-		var result = body();
-	} catch(e) {
-		return recover(e);
-	}
-	if (result && result.then) {
-		return result.then(void 0, recover);
-	}
-	return result;
-}
-
-// Asynchronously await a promise and pass the result to a finally continuation
-function _finallyRethrows(body, finalizer) {
-	try {
-		var result = body();
-	} catch (e) {
-		return finalizer(true, e);
-	}
-	if (result && result.then) {
-		return result.then(finalizer.bind(null, false), finalizer.bind(null, true));
-	}
-	return finalizer(false, result);
-}
-
-var useAsync = function useAsync(asyncFn) {
-  var _useState = useState(null),
-    data = _useState[0],
-    setData = _useState[1];
-  var _useState2 = useState(null),
-    error = _useState2[0],
-    setError = _useState2[1];
-  var _useState3 = useState(false),
-    isLoading = _useState3[0],
-    setIsLoading = _useState3[1];
-  useEffect(function () {
+const useAsync = asyncFn => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
     setData(null);
     setError(null);
     setIsLoading(false);
-    var fetchData = function fetchData() {
+    const fetchData = async () => {
+      setIsLoading(true);
       try {
-        setIsLoading(true);
-        var _temp = _finallyRethrows(function () {
-          return _catch(function () {
-            return Promise.resolve(asyncFn()).then(function (result) {
-              setData(result);
-            });
-          }, function (err) {
-            setError(err);
-          });
-        }, function (_wasThrown, _result) {
-          setIsLoading(false);
-          if (_wasThrown) throw _result;
-          return _result;
-        });
-        return Promise.resolve(_temp && _temp.then ? _temp.then(function () {}) : void 0);
-      } catch (e) {
-        return Promise.reject(e);
+        const result = await asyncFn();
+        setData(result);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchData();
   }, [asyncFn]);
   return {
-    data: data,
-    error: error,
+    data,
+    error,
     loading: isLoading
   };
 };
 
-var useCached = function useCached(value) {
-  var ref = useRef();
-  useEffect(function () {
+const useCached = value => {
+  const ref = useRef();
+  useEffect(() => {
     if (!(isEmpty(value) || isUndefined(value) || isNull(value))) {
       ref.current = value;
     }
@@ -87,171 +39,138 @@ var useCached = function useCached(value) {
   return ref.current;
 };
 
-var useCountdown = function useCountdown(_ref) {
-  var initialRemainingTimeMs = _ref.initialRemainingTimeMs,
-    onEnd = _ref.onEnd,
-    onTick = _ref.onTick,
-    _ref$countdownStepMs = _ref.countdownStepMs,
-    countdownStepMs = _ref$countdownStepMs === void 0 ? 1000 : _ref$countdownStepMs;
-  var _useState = useState(initialRemainingTimeMs),
-    timeLeft = _useState[0],
-    setTimeLeft = _useState[1];
-  var _useState2 = useState(false),
-    isPaused = _useState2[0],
-    setIsPaused = _useState2[1];
-  var timerRef = useRef(null);
-  var startTimer = useCallback(function () {
+const useCountdown = ({
+  initialRemainingTimeMs,
+  onEnd,
+  onTick,
+  countdownStepMs: _countdownStepMs = 1000
+}) => {
+  const [timeLeft, setTimeLeft] = useState(initialRemainingTimeMs);
+  const [isPaused, setIsPaused] = useState(false);
+  const timerRef = useRef(null);
+  const startTimer = useCallback(() => {
     if (!timerRef.current) {
-      timerRef.current = setInterval(function () {
-        setTimeLeft(function (prev) {
-          if (prev <= countdownStepMs) {
+      timerRef.current = setInterval(() => {
+        setTimeLeft(prev => {
+          if (prev <= _countdownStepMs) {
             clearInterval(timerRef.current);
             timerRef.current = null;
             onEnd === null || onEnd === void 0 ? void 0 : onEnd();
             return 0;
           }
-          var newTime = prev - countdownStepMs;
+          const newTime = prev - _countdownStepMs;
           onTick === null || onTick === void 0 ? void 0 : onTick(newTime);
           return newTime;
         });
-      }, countdownStepMs);
+      }, _countdownStepMs);
     }
-  }, [countdownStepMs, onEnd, onTick]);
-  var stopTimer = useCallback(function () {
+  }, [_countdownStepMs, onEnd, onTick]);
+  const stopTimer = useCallback(() => {
     if (timerRef.current) {
       clearInterval(timerRef.current);
       timerRef.current = null;
     }
   }, []);
-  useEffect(function () {
+  useEffect(() => {
     if (timeLeft > 0 && !isPaused && !timerRef.current) {
       startTimer();
     }
-    return function () {
+    return () => {
       stopTimer();
     };
   }, [timeLeft, isPaused, startTimer, stopTimer]);
-  var pauseCountdown = useCallback(function () {
+  const pauseCountdown = useCallback(() => {
     setIsPaused(true);
     stopTimer();
   }, [stopTimer]);
-  var resumeCountdown = useCallback(function () {
+  const resumeCountdown = useCallback(() => {
     setIsPaused(false);
     startTimer();
   }, [startTimer]);
-  var setNewRemainingTime = useCallback(function (newTimeMs) {
+  const setNewRemainingTime = useCallback(newTimeMs => {
     setTimeLeft(newTimeMs);
   }, []);
-  var resetCountdown = useCallback(function () {
+  const resetCountdown = useCallback(() => {
     setTimeLeft(0);
     pauseCountdown();
   }, [pauseCountdown]);
   return {
-    isPaused: isPaused,
-    timeLeft: timeLeft,
-    setNewRemainingTime: setNewRemainingTime,
-    resetCountdown: resetCountdown,
-    pauseCountdown: pauseCountdown,
-    resumeCountdown: resumeCountdown
+    isPaused,
+    timeLeft,
+    setNewRemainingTime,
+    resetCountdown,
+    pauseCountdown,
+    resumeCountdown
   };
 };
 
-var useDebouncedValue = function useDebouncedValue(value, delay) {
-  var _useState = useState(value),
-    debouncedValue = _useState[0],
-    setDebouncedValue = _useState[1];
-  useEffect(function () {
-    var handler = setTimeout(function () {
+const useDebouncedValue = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    const handler = setTimeout(() => {
       setDebouncedValue(value);
     }, delay);
-    return function () {
+    return () => {
       clearTimeout(handler);
     };
   }, [value, delay]);
   return debouncedValue;
 };
 
-var useDeepCompareEffect = function useDeepCompareEffect(callback, dependencies) {
-  var currentDependenciesRef = useRef(null);
+const useDeepCompareEffect = (callback, dependencies) => {
+  const currentDependenciesRef = useRef(null);
   if (!isEqual(currentDependenciesRef.current, dependencies)) {
     currentDependenciesRef.current = dependencies;
   }
   useEffect(callback, [currentDependenciesRef.current]);
 };
 
-function _extends() {
-  return _extends = Object.assign ? Object.assign.bind() : function (n) {
-    for (var e = 1; e < arguments.length; e++) {
-      var t = arguments[e];
-      for (var r in t) ({}).hasOwnProperty.call(t, r) && (n[r] = t[r]);
-    }
-    return n;
-  }, _extends.apply(null, arguments);
-}
-
-var DEFAULT_HEADERS = {
+const DEFAULT_HEADERS = {
   'Content-Type': 'application/json'
 };
-var useFetch = function useFetch(url, options) {
-  if (options === void 0) {
-    options = {};
+const useFetch = (url, options = {}) => useAsync(() => fetch(url, {
+  ...options,
+  headers: {
+    ...DEFAULT_HEADERS,
+    ...(options.headers ?? {})
   }
-  return useAsync(function () {
-    var _options$headers;
-    return fetch(url, _extends({}, options, {
-      headers: _extends({}, DEFAULT_HEADERS, (_options$headers = options.headers) != null ? _options$headers : {})
-    })).then(function (res) {
-      if (res.ok) {
-        return res.json();
-      }
-      return res.json().then(function (json) {
-        throw new Error(json);
-      });
-    });
+}).then(res => {
+  if (res.ok) {
+    return res.json();
+  }
+  return res.json().then(json => {
+    throw new Error(json);
   });
-};
+}));
 
-var useMount = function useMount(func) {
-  return useEffect(func, []);
-};
+const useMount = func => useEffect(func, []);
 
-var usePrevious = function usePrevious(value, valuesToIgnore) {
-  if (valuesToIgnore === void 0) {
-    valuesToIgnore = [];
-  }
-  var ref = useRef();
-  useEffect(function () {
+const usePrevious = (value, valuesToIgnore = []) => {
+  const ref = useRef();
+  useEffect(() => {
     if (valuesToIgnore.includes(value)) return;
     ref.current = value;
   }, [value, valuesToIgnore]);
   return ref.current;
 };
 
-var useSpecificKeyExtractor = function useSpecificKeyExtractor(prefix, field) {
-  return useCallback(function (item, index) {
-    return prefix + "-" + item[field] + "-" + index;
-  }, [prefix, field]);
-};
+const useSpecificKeyExtractor = (prefix, field) => useCallback((item, index) => `${prefix}-${item[field]}-${index}`, [prefix, field]);
 
-var useStateWithCached = function useStateWithCached(initialValue) {
-  var _useState = useState(initialValue),
-    state = _useState[0],
-    setState = _useState[1];
-  var cachedState = useCached(state);
+const useStateWithCached = initialValue => {
+  const [state, setState] = useState(initialValue);
+  const cachedState = useCached(state);
   return [state, setState, cachedState];
 };
 
-var useStateWithPrevious = function useStateWithPrevious(initialValue) {
-  var _useState = useState(initialValue),
-    state = _useState[0],
-    setState = _useState[1];
-  var prevState = usePrevious(state);
+const useStateWithPrevious = initialValue => {
+  const [state, setState] = useState(initialValue);
+  const prevState = usePrevious(state);
   return [state, setState, prevState];
 };
 
-var useUpdateEffect = function useUpdateEffect(callback, deps) {
-  var firstRenderRef = useRef(true);
-  useEffect(function () {
+const useUpdateEffect = (callback, deps) => {
+  const firstRenderRef = useRef(true);
+  useEffect(() => {
     if (firstRenderRef.current) {
       firstRenderRef.current = false;
       return;
@@ -260,28 +179,18 @@ var useUpdateEffect = function useUpdateEffect(callback, deps) {
   }, deps);
 };
 
-var useStateWithValidation = function useStateWithValidation(initialValue, validator) {
-  var _useState = useState(initialValue),
-    state = _useState[0],
-    setState = _useState[1];
-  var _useState2 = useState(validator(state)),
-    isValid = _useState2[0],
-    setIsValid = _useState2[1];
-  useUpdateEffect(function () {
+const useStateWithValidation = (initialValue, validator) => {
+  const [state, setState] = useState(initialValue);
+  const [isValid, setIsValid] = useState(validator(state));
+  useUpdateEffect(() => {
     setIsValid(validator(state));
   }, [state, validator]);
   return [state, setState, isValid];
 };
 
-var useToggle = function useToggle(defaultValue) {
-  var _useState = useState(defaultValue),
-    isValue = _useState[0],
-    setIsValue = _useState[1];
-  var toggleValue = useCallback(function () {
-    return setIsValue(function (prevState) {
-      return !prevState;
-    });
-  }, []);
+const useToggle = defaultValue => {
+  const [isValue, setIsValue] = useState(defaultValue);
+  const toggleValue = useCallback(() => setIsValue(prevState => !prevState), []);
   return [isValue, toggleValue];
 };
 
